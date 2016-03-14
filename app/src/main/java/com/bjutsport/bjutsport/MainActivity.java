f13b54bc23cb5f3b3d07556dfcba6cb2db150307
shaolizhi1234 <674940850@qq.com>
2016-03-14 22:46:36 +0800

    怎么没改完？

--- app/src/main/java/com/bjutsport/bjutsport/MainActivity.java
+++ app/src/main/java/com/bjutsport/bjutsport/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bjutsport.bjutsport;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -8,7 +9,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
