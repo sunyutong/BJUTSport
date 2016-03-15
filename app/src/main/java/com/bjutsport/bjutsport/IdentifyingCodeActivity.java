ddc6a78fa29b9f775689badf94edc0c46d68bb16
Hudiyu <767785874@qq.com>
2016-03-15 22:22:15 +0800

    添加短信验证功能

---
+++ app/src/main/java/com/bjutsport/bjutsport/IdentifyingCodeActivity.java
@@ -0,0 +1,13 @@
+package com.bjutsport.bjutsport;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IdentifyingCodeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identifying_code);
+    }
+}
