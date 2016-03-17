58d7a9c605b62da50558726812bf613050474593
shaolizhi1234 <674940850@qq.com>
2016-03-18 01:47:13 +0800

    针对MainActivity、LoginActivity、RegisterActivity代码的一优化优化
    主要添加了一些注释，修改一些错误拼写，优化了一些UI设计。
    由于网络原因，无法进行黑盒测试。故没有继续往下更改代码。
    目前还存在的尚未修改的代码有：
    1. 注册成功后，应该直接跳转到主界面
    2. 主界面的自动登录
    3. 验证手机号页面的错误提示Toast
    4. 将TextView是否为空等类似判断做回本地，将网络负担降低到最小
    5. 将Toast字号调整到与整体风格一致
    6. 顶边栏颜色改变（不要黑的）
    7. 密码输入两次改变为输入一次
    8. 在登陆成功后Finish掉之前的全部Activity

--- app/src/main/java/com/bjutsport/bjutsport/MainActivity.java
+++ app/src/main/java/com/bjutsport/bjutsport/MainActivity.java
@@ -9,29 +9,35 @@ import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    /** 控件绑定 */
+
+        //登录按钮
+        Button buttonLogin = (Button) findViewById(R.id.Button_MainActivity_Login);
+        //注册按钮
+        Button buttonRegister = (Button) findViewById(R.id.Button_MainActivity_Register);
+        //跳过登陆按钮
+        Button buttonSkipLogin = (Button) findViewById(R.id.Button_MainActivity_Skip_Login);
+
+    /** UI设定 */
+
+        // 透明状态栏
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            // 透明状态栏
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
+        // 半透明登录按钮
+        buttonLogin.getBackground().setAlpha(230);//0~255透明度值
+        // 半透明注册按钮
+        buttonRegister.getBackground().setAlpha(230);//0~255透明度值
 
-        //设置登录按钮的透明度值
-        View button_login = findViewById(R.id.Button_MainActivity_Login);//找到你要设透明背景的layout 的id
-        button_login.getBackground().setAlpha(230);//0~255透明度值
-        //设置注册按钮的透明度值
-        View button_register = findViewById(R.id.Button_MainActivity_Register);//找到你要设透明背景的layout 的id
-        button_register.getBackground().setAlpha(230);//0~255透明度值
+    /** 点击事件 */
 
-        //获取登录按钮
-        Button ButtonLogin = (Button) findViewById(R.id.Button_MainActivity_Login);
-        ButtonLogin.setOnClickListener(new View.OnClickListener() {
-
-            //点击进入登录界面
+        //点击登录按钮->进入LoginActivity
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent_login = new Intent(MainActivity.this, LoginActivity.class);
@@ -39,11 +45,8 @@ public class MainActivity extends Activity {
             }
         });
 
-        //获取注册按钮
-        Button ButtonRegister = (Button) findViewById(R.id.Button_MainActivity_Register);
-        ButtonRegister.setOnClickListener(new View.OnClickListener() {
-
-            //点击进入注册界面
+        //点击注册按钮->进入VerificationActivity
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent_verification = new Intent(MainActivity.this, VerificationActivity.class);
@@ -56,17 +59,14 @@ public class MainActivity extends Activity {
             }
         });
 
-        //获得跳过登陆按钮
-        Button ButtonSkipLogin = (Button) findViewById(R.id.Button_MainActivity_Skip_Login);
-        ButtonSkipLogin.setOnClickListener(new View.OnClickListener() {
-
-            //点击进入注册界面
+        //点击跳过登录按钮->进入UserActivity界面
+        buttonSkipLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent_register = new Intent(MainActivity.this, UserActivity.class);
                 startActivity(intent_register);
             }
         });
-    }
 
+    }
 }
--- app/src/main/java/com/bjutsport/bjutsport/RegisterActivity.java
+++ app/src/main/java/com/bjutsport/bjutsport/RegisterActivity.java
@@ -23,52 +23,55 @@ import com.bjutsport.aes.AESUtil;
 import java.net.SocketTimeoutException;
 
 public class RegisterActivity extends Activity {
-
+    /**
+     * 静态常量
+     */
+    //AES密钥
     private static final String AES_KEY = "BJUTSport1234567";
+    //注册界面URL
     private static final String WEBSERVICE_WSDL_URL = "http://192.168.1.101:8080/BJUTSport/services/RegisterImplPort?wsdl";
+    //注册界面NameSpace
     private static final String WEBSERVICE_NAMESPACE = "http://register.bjutsport.com/";
+    //注册界面方法名称:register
     private static final String METHOD_NAME = "register";
 
-    private static final int SHOW_PASSWORD_UNCONSISTENT = 0x0000;
+    //显示代码前后不一致
+    private static final int SHOW_PASSWORD_INCONSISTENT = 0x0000;
+    //显示注册成功
     private static final int SHOW_REGISTER_SUCCESS = 0x0001;
+    //显示注册失败
     private static final int SHOW_REGISTER_FAILED = 0x0002;
-    private static final int SHOW_SOCKETTIMOUT = 0x0003;
+    //显示连接超时
+    private static final int SHOW_SOCKET_TIMEOUT = 0x0003;
+    //显示密码长度过短
     private static final int SHOW_PASSWORD_TOO_SHORT = 0x0004;
-    private static final int JUMP_TO_LOGINACTIVITY = 0x0005;
+    //跳转到LoginActivity
+    private static final int JUMP_TO_LOGIN_ACTIVITY = 0x0005;
 
+    //密码长度:8位
     private static final int passwordLength = 8;
 
+    //注册成功
     private static final int REGISTER_SUCCESS = 1;
+    //注册失败
     private static final int REGISTER_FAILED = 0;
 
+    /**
+     * UI线程 (主线程)
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //设置标题为空
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         setContentView(R.layout.activity_register);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            // 透明状态栏
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        }
-
-        //获取返回按钮
+        /**
+         * 控件绑定、Handler、Bundle定义
+         * */
+        //后退按钮
         Button button_back = (Button) findViewById(R.id.Button_RegisterActivity_to_MainActivity);
-        //点击返回核实界面
-        button_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent_Verification = new Intent(RegisterActivity.this, VerificationActivity.class);
-                startActivity(intent_Verification);
-                finish();
-            }
-        });
-
-        final Bundle bundle = this.getIntent().getExtras();
-
+        //注册按钮
+        Button button_register = (Button) findViewById(R.id.Button_Register_Verification);
         //Register的Handler
         final Handler registerHandler = new Handler() {
             @Override
@@ -78,7 +81,7 @@ public class RegisterActivity extends Activity {
                         //显示密码太短
                         Toast.makeText(getApplicationContext(), "密码长度应至少大于8", Toast.LENGTH_SHORT).show();
                         break;
-                    case SHOW_PASSWORD_UNCONSISTENT:
+                    case SHOW_PASSWORD_INCONSISTENT:
                         //显示密码前后不一致
                         Toast.makeText(getApplicationContext(), "密码前后不一致", Toast.LENGTH_SHORT).show();
                         break;
@@ -90,11 +93,11 @@ public class RegisterActivity extends Activity {
                         //显示注册失败
                         Toast.makeText(getApplicationContext(), "用户名已存在,注册失败", Toast.LENGTH_SHORT).show();
                         break;
-                    case SHOW_SOCKETTIMOUT:
+                    case SHOW_SOCKET_TIMEOUT:
                         //显示连接超时
                         Toast.makeText(getApplicationContext(), "连接超时,请检查网络连接", Toast.LENGTH_SHORT).show();
                         break;
-                    case JUMP_TO_LOGINACTIVITY:
+                    case JUMP_TO_LOGIN_ACTIVITY:
                         //跳转到用户界面
                         Intent intent_User = new Intent(RegisterActivity.this, LoginActivity.class);
                         startActivity(intent_User);
@@ -105,32 +108,63 @@ public class RegisterActivity extends Activity {
                 }
             }
         };
+        //从VerificationActivity传过来的Bundle，其中包含了用户名(手机号)信息
+        final Bundle bundle = this.getIntent().getExtras();
 
-        //获取注册按钮
-        Button button_register = (Button) findViewById(R.id.Button_Register_Verification);
+
+
+        /**
+         * UI设定
+         * */
+        //设置状态栏为透明
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
+        /**
+         * 点击事件
+         * */
+        //点击返回按钮->回到VerificationActivity
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //点击注册按钮，创建新的线程以进行网络访问
         button_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //创建新的进程以进行网络访问
+                /**
+                 * 子线程
+                 * */
                 new Thread(new Runnable() {
+                    /**
+                     * 子线程控件绑定
+                     * */
+                    //密码输入栏
+                    EditText ediUserPassword = (EditText) findViewById(R.id.EditText_Register_userPassword);
+                    //密码输入确认栏
+                    EditText ediUserPasswordCheck = (EditText) findViewById(R.id.EditText_Register_userPassword_Check);
+                    /**
+                     * 子线程开始执行
+                     * */
                     @Override
                     public void run() {
-                        //获得密码,二次输入密码EditText
-                        EditText ediUserPassword, ediUserPasswordCheck;
-                        ediUserPassword = (EditText) findViewById(R.id.EditText_Register_userPassword);
-                        ediUserPasswordCheck = (EditText) findViewById(R.id.EditText_Register_userPassword_Check);
-
-                        //提取用户输入的用户名,密码和二次密码
-                        String strUserName = bundle.getString("phoneNums");
-                        //String strUserName = "15911135092";
+                        //提取用户输入的用户名
+                        String strUserName = bundle.getString("phoneNums");//String strUserName = "15911135092";
+                        //提取用户输入的密码
                         String strUserPassword = ediUserPassword.getText().toString();
+                        //提取用户输入的和二次密码
                         String strUserPasswordCheck = ediUserPasswordCheck.getText().toString();
+
                         if (!checkPasswordLength(strUserPassword)) {
                             //如果用户输入的密码长度小于8,发送消息显示
                             registerHandler.sendEmptyMessage(SHOW_PASSWORD_TOO_SHORT);
                         } else if (!strUserPassword.equals(strUserPasswordCheck)) {
                             //如果用户输入的密码前后不一致,发送消息显示密码前后不一致
-                            registerHandler.sendEmptyMessage(SHOW_PASSWORD_UNCONSISTENT);
+                            registerHandler.sendEmptyMessage(SHOW_PASSWORD_INCONSISTENT);
                         } else {
                             //否则进行注册
                             try {
@@ -170,10 +204,10 @@ public class RegisterActivity extends Activity {
                                         //如果服务器返回值为true,则发送消息以显示注册成功
                                         registerHandler.sendEmptyMessage(SHOW_REGISTER_SUCCESS);
                                         Thread.sleep(300);
-                                        registerHandler.sendEmptyMessage(JUMP_TO_LOGINACTIVITY);
+                                        registerHandler.sendEmptyMessage(JUMP_TO_LOGIN_ACTIVITY);
                                         break;
                                     case REGISTER_FAILED:
-                                        //如果服务器返回值为flase,则发送消息以显示注册失败
+                                        //如果服务器返回值为false,则发送消息以显示注册失败
                                         registerHandler.sendEmptyMessage(SHOW_REGISTER_FAILED);
                                         break;
                                     default:
@@ -182,7 +216,7 @@ public class RegisterActivity extends Activity {
 
                             } catch (SocketTimeoutException ste) {
                                 //抛出异常以显示连接超时
-                                registerHandler.sendEmptyMessage(SHOW_SOCKETTIMOUT);
+                                registerHandler.sendEmptyMessage(SHOW_SOCKET_TIMEOUT);
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
@@ -193,8 +227,10 @@ public class RegisterActivity extends Activity {
         });
     }
 
+    //代码长度检查函数
     private boolean checkPasswordLength(String password) {
         return password.length() >= passwordLength;
     }
+
 
 }
