a5dd3c92e43875077e855a4374f82748ca2fd263
shaolizhi1234 <674940850@qq.com>
2016-04-09 12:51:50 +0800

    侧滑一版

--- app/src/main/java/com/bjutsport/bjutsport/UserActivity.java
+++ app/src/main/java/com/bjutsport/bjutsport/UserActivity.java
@@ -1,19 +1,23 @@
 package com.bjutsport.bjutsport;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
-import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.TextView;
 
 import com.bjutsport.view.SlidingMenu;
 
-public class UserActivity extends Activity {
+public class UserActivity extends BaseActivity implements View.OnTouchListener, GestureDetector.OnGestureListener{
 
-    private SlidingMenu mLeftMenu;
 
+    //创建一个用于识别收拾的GestureDetector对象
+    private GestureDetector detector = new GestureDetector(this);
+    //滑动菜单
+    private SlidingMenu userLayout;
     //搜索按钮
     TextView textViewSearch;
 
@@ -22,21 +26,91 @@ public class UserActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_user);
-
-        mLeftMenu = (SlidingMenu) findViewById(R.id.Menu_UserActivity);
+        userLayout = (SlidingMenu)findViewById(R.id.Menu_UserActivity);
         textViewSearch= (TextView) findViewById(R.id.TextView_UserActivity_To_SearchActivity);
-
         textViewSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent_search = new Intent(UserActivity.this,SearchActivity.class);
+                Intent intent_search = new Intent(UserActivity.this, SearchActivity.class);
                 startActivity(intent_search);
             }
         });
+
+
+        userLayout.setOnTouchListener(this);
+        userLayout.setLongClickable(true);
+        detector.setIsLongpressEnabled(true);
     }
 
+
     public void toggleMenu(View view) {
-        mLeftMenu.toggle();
+        userLayout.toggle();
+    }
+
+    //重写OnTouchListener的onTouch方法
+    //此方法在触摸屏被触摸，即发生触摸事件（接触和抚摸两个事件，挺形象）的时候被调用。
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        detector.onTouchEvent(event);
+        return true;
+    }
+
+    //在按下动作时被调用
+    @Override
+    public boolean onDown(MotionEvent e) {
+
+        return false;
+    }
+
+    //在抛掷动作时被调用
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                           float velocityY) {
+        //velocityX表示横向的移动，根据手指移动的方向切换女孩
+        if(velocityX < 0){
+            userLayout.closeMenu();
+        }else if (velocityX > 0){
+            userLayout.openMenu();
+        }
+        return false;
+    }
+
+    //在长按时被调用
+    @Override
+    public void onLongPress(MotionEvent e) {
+    }
+
+    //在滚动时调用
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+                            float distanceY) {
+        return false;
+    }
+
+    //在按住时被调用
+    @Override
+    public void onShowPress(MotionEvent e) {
+    }
+
+    //在抬起时被调用
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        if(inRangeOfView(userLayout.mContent,e)){
+            userLayout.closeMenu();
+        }
+        return false;
+    }
+
+    //判断点击的位置是不是在Content上
+    private boolean inRangeOfView(View view, MotionEvent ev){
+        int[] location = new int[2];
+        view.getLocationOnScreen(location);
+        int x = location[0];
+        int y = location[1];
+        if(ev.getX() < x || ev.getX() > (x + view.getWidth()) || ev.getY() < y || ev.getY() > (y + view.getHeight())){
+            return false;
+        }
+        return true;
     }
 
 }
--- app/src/main/java/com/bjutsport/view/SlidingMenu.java
+++ app/src/main/java/com/bjutsport/view/SlidingMenu.java
@@ -6,7 +6,6 @@ import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.MotionEvent;
-import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.HorizontalScrollView;
@@ -21,41 +20,28 @@ import com.nineoldandroids.view.ViewHelper;
  */
 public class SlidingMenu extends HorizontalScrollView {
 
+    //横向水平滚动条中肯定有个LinearLayout
     private LinearLayout mWapper;
+    //菜单栏是个ViewGroup的子类
     private ViewGroup mMenu;
-    private ViewGroup mContent;
+    //内容区域：主界面
+    public ViewGroup mContent;
+    //手机屏幕的宽度
     private int mScreenWidth;
-
     private int mMenuWidth;
-
-    //dp
+    //Menu与右侧的距离
     private int mMenuRightPadding = 50;
-
     private boolean once;
-
     private boolean isOpen;
 
-    /**
-     * 未使用自定义属性时，调用
-     *
-     * @param context 拉屎
-     * @param attrs   大便
-     */
+    public SlidingMenu(Context context) {
+        this(context, null);
+    }
     public SlidingMenu(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
-
     }
-
-    /**
-     * 当使用了自定义属性时，会调用此构造方法
-     *
-     * @param context
-     * @param attrs
-     * @param defStyle
-     */
     public SlidingMenu(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-
         //获取我们定义的属性
         TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SlidingMenu, defStyle, 0);
         int n = a.getIndexCount();
@@ -70,16 +56,11 @@ public class SlidingMenu extends HorizontalScrollView {
 
         //TypedArray需要释放
         a.recycle();
-
-
+        //获取手机屏幕的宽度
         WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics outMetrics = new DisplayMetrics();
         windowManager.getDefaultDisplay().getMetrics(outMetrics);
         mScreenWidth = outMetrics.widthPixels;
-    }
-
-    public SlidingMenu(Context context) {
-        this(context, null);
     }
 
     /**
@@ -91,31 +72,30 @@ public class SlidingMenu extends HorizontalScrollView {
      */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
         if (!once) {
+            //滚动条里面也就一个元素
             mWapper = (LinearLayout) getChildAt(0);
+            //LinearLayout里面的第一个元素：菜单
             mMenu = (ViewGroup) mWapper.getChildAt(0);
+            //LinearLayout里面的第二个元素:主界面
             mContent = (ViewGroup) mWapper.getChildAt(1);
+            //高度是match_parent，不需要显式设置。但是宽度，这里显式设置一下。
+            //menu的宽度就等于屏幕的宽度减去右侧保留的宽度
             mMenuWidth = mMenu.getLayoutParams().width = mScreenWidth - mMenuRightPadding;
+            //显式设置一下Content的宽度——就是屏幕的宽度
             mContent.getLayoutParams().width = mScreenWidth;
             once = true;
         }
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
-    /**
-     * 通过设置偏移量，将menu隐藏
-     *
-     * @param changed
-     * @param l
-     * @param t
-     * @param r
-     * @param b
-     */
-    @Override
+    //我们需要这样一个效果：即先显示Content，menu在左侧隐藏。所以在这里设置一个偏移量。
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
+        //为了防止多次调用
         if (changed) {
+            //scrollTO(x,y)使用方法说明：x为正值时，滚动条向右移动，内容区域向左移动
+            //移动一个menu的宽度，这样就正好将其隐藏住了
             this.scrollTo(mMenuWidth, 0);
         }
     }
@@ -125,10 +105,11 @@ public class SlidingMenu extends HorizontalScrollView {
         int action = ev.getAction();
         switch (action) {
             case MotionEvent.ACTION_UP:
-                //隐藏在左边的宽度
+                //隐藏在左边的宽度就是getScrollX
                 int scrollX = getScrollX();
 
-                if (scrollX >= (mMenuWidth / 2)) {
+                if (scrollX >= (mMenuWidth * 3 / 4)) {
+                    //ScrollTO是瞬间完成，smoothScrollTo是有一个动画的感觉
                     this.smoothScrollTo(mMenuWidth, 0);
                     isOpen = false;
                 } else {
@@ -137,25 +118,20 @@ public class SlidingMenu extends HorizontalScrollView {
                 }
                 return true;
         }
-
         return super.onTouchEvent(ev);
     }
 
-    /**
-     * 打开菜单
-     */
     public void openMenu() {
         if (isOpen) return;
         this.smoothScrollTo(0, 0);
         isOpen = true;
     }
-
     public void closeMenu() {
         if (!isOpen) return;
+        //ScrollTO是瞬间完成，smoothScrollTo是有一个动画的感觉
         this.smoothScrollTo(mMenuWidth, 0);
         isOpen = false;
     }
-
     /**
      * 切换菜单
      */
@@ -166,15 +142,14 @@ public class SlidingMenu extends HorizontalScrollView {
             openMenu();
         }
     }
-
-    /**
-     * 滚动发生时
-     */
-    @Override
-    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
-        super.onScrollChanged(l, t, oldl, oldt);
-        float scale = l * 1.0f / mMenuWidth;
-        //调用属性动画，设置TranslationX
-        ViewHelper.setTranslationX(mMenu, mMenuWidth * scale);
-    }
+//    /**
+//     * 抽屉式菜单
+//     */
+//    @Override
+//    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+//        super.onScrollChanged(l, t, oldl, oldt);
+//        float scale = l * 1.0f / mMenuWidth;
+//        //调用属性动画，设置TranslationX
+//        ViewHelper.setTranslationX(mMenu, mMenuWidth * scale);
+//    }
 }
